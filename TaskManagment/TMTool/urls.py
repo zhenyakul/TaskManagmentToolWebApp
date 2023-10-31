08d50fd36227e5f5be18b2008e656454e92a48fe
yanislavK <121428054+yanislavK@users.noreply.github.com>
2023-10-31 08:31:54 +0100

    add models

--- TaskManagment/TMTool/urls.py
+++ TaskManagment/TMTool/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     #Topic-page
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    #Create-topic-page
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #Create-entry-page
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry')
 ]
