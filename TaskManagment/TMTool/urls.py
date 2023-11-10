7662ddd59da70f8bf745140210b69a72d4f12024
Yevhenii <yevhenii.kul@gmail.com>
2023-11-10 14:58:10 +0100

    Add edit entry

--- TaskManagment/TMTool/urls.py
+++ TaskManagment/TMTool/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     #Topic-page
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    #Create-topic-page
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #Create-entry-page
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    #Edit-entry-page
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+
 ]
--- TaskManagment/TMTool/views.py
+++ TaskManagment/TMTool/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm
+from .forms import EntryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     return render(request, 'TMTool/index.html')
@@ -14,3 +18,42 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'TMTool/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('TMTool:topics')
+
+    context = {'form': form}
+    return render(request, 'TMTool/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('TMTool:topic', topic_id=topic_id)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'TMTool/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('TMTool:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'TMTool/edit_entry.html', context)
