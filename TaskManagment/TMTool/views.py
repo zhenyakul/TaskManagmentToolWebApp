bc9cd5405473fb54cda05894801cc290939e3b3d
Yevhenii <yevhenii.kul@gmail.com>
2023-11-26 16:41:33 +0100

    add decorators

--- TaskManagment/TMTool/views.py
+++ TaskManagment/TMTool/views.py
@@ -1,36 +1,45 @@
-from django.shortcuts import render
 from .models import Topic, Entry
 from .forms import TopicForm
 from .forms import EntryForm
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 def index(request):
     return render(request, 'TMTool/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'TMTool/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'TMTool/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('TMTool:topics')
 
     context = {'form': form}
     return render(request, 'TMTool/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -45,9 +54,12 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'TMTool/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
--- TaskManagment/TaskManagment/settings.py
+++ TaskManagment/TaskManagment/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#my settings
+LOGIN_URL = '/users/login/'
+LOGOUT_REDIRECT_URL = '/'
