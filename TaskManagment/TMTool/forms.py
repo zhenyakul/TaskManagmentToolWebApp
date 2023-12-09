066b80420f499a8b56eb0cc740c4c9ba290abfb9
yanislavK <121428054+yanislavK@users.noreply.github.com>
2023-12-09 14:29:44 +0100

    styles

--- TaskManagment/TMTool/forms.py
+++ TaskManagment/TMTool/forms.py
@@ -2,18 +2,21 @@ from django import forms
 from .models import Topic
 from .models import Entry
 
+
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
-        fields = ['text']
-        labels = {'text': ''}
-        widgets = {'text': forms.TextInput(attrs={'class': 'charfield'})}
+        fields = ["text"]
+        labels = {"text": ""}
+        widgets = {"text": forms.TextInput(attrs={"class": "add-topic-input"})}
+
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ['text', 'flag_status']
-        labels = {'text': 'Entry:', 'flag_status': 'Change status:'}
-        widgets = {'text': forms.Textarea(attrs={'cols': 100, 'class': 'textField'}),
-                   'flag_status': forms.Select(attrs={'class': 'select'})}
-        
+        fields = ["text", "flag_status"]
+        labels = {"text": "Entry:", "flag_status": "Change status:"}
+        widgets = {
+            "text": forms.Textarea(attrs={"cols": 100, "class": "textField"}),
+            "flag_status": forms.Select(attrs={"class": "select"}),
+        }
