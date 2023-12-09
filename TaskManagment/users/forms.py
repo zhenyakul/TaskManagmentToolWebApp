38eee501da93b8ba4ecf6a1b6d867f1b436b1b1b
Yevhenii <yevhenii.kul@gmail.com>
2023-12-09 13:52:08 +0100

    recreating registration

---
+++ TaskManagment/users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {'username': 'Username:', 'email': 'Email:', 'password1': 'Password:', 'password2': 'Confirm your password:'}
+        widgets = {'username': forms.TextInput(attrs={'class': 'usernameInput'}), 'email': forms.EmailInput(), 'password': forms.PasswordInput()}
