2b9193708f78c4646989251c913756159a7be998
yanislavK <121428054+yanislavK@users.noreply.github.com>
2023-12-11 21:28:17 +0100

    completed styles

--- TaskManagment/TMTool/migrations/0006_entry_flag_status.py
+++ TaskManagment/TMTool/migrations/0006_entry_flag_status.py
@@ -4,15 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('TMTool', '0005_remove_entry_owner_topic_owner'),
+        ("TMTool", "0005_remove_entry_owner_topic_owner"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='entry',
-            name='flag_status',
-            field=models.CharField(choices=[('status1', 'created'), ('status2', 'In progress'), ('status3', 'suspended'), ('status4', 'done')], default='status1', max_length=10),
+            model_name="entry",
+            name="flag_status",
+            field=models.CharField(
+                choices=[
+                    ("status1", "Created"),
+                    ("status2", "In progress"),
+                    ("status3", "Suspended"),
+                    ("status4", "Done"),
+                ],
+                default="status1",
+                max_length=15,
+            ),
         ),
     ]
